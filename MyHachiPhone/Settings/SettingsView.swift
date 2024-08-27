0592295ddb9319ee059fd6d3384b3ab421a10e2d
me990928 <me990928@gmail.com>
2024-08-28 06:41:46 +0900

    add view function

--- MyHachiPhone/Settings/SettingsView.swift
+++ MyHachiPhone/Settings/SettingsView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct SettingsView: View {
     
     @Environment(\.colorScheme) var colorScheme
+    @ObservedObject var SettingVM = SettingsViewModel()
     
     @State var edit: Bool = false
     
     @State var wage: String = ""
-    let wagePlaceholder: String
-    @AppStorage ("teate") var teate: Bool = false
+    @State var wagePlaceholder: String
+    @State var teate: Bool
+    
+    init(){
+        self.wagePlaceholder = UserDefaultsMan().loadUserDefaultString(key: "wage")
+        self.teate = UserDefaultsMan().loadUserDefaultBool(key: "teate")
+    }
     
     var body: some View {
         VStack {
@@ -29,13 +35,31 @@ struct SettingsView: View {
                 }
                 Spacer()
                 Button(action: {
-                    withAnimation {
-                        edit.toggle()
+                    if edit {
+                        SettingVM.editTeate(teate: teate)
+                        if !wage.isEmpty {
+                            SettingVM.editWage(wage: wage){
+                                wagePlaceholder = UserDefaults().string(forKey: "wage") ?? "1100"
+                                withAnimation {
+                                    wage = ""
+                                }
+                            }
+                        }
+                        withAnimation {
+                            edit.toggle()
+                        }
+                    } else {
+                        withAnimation {
+                            edit.toggle()
+                        }
                     }
                 }, label: {
+                    
                     Text(edit ? "保存" : "編集")
+                    
                 }).buttonStyle(BorderedProminentButtonStyle())
-            }.padding(.trailing)
+                
+            }.padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
             List {
                 HStack{
                     Text("時給")
@@ -52,5 +76,5 @@ struct SettingsView: View {
 }
 
 #Preview {
-    SettingsView(wagePlaceholder: "1100")
+    SettingsView()
 }
