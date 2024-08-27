ba20b9d32aa7f17df40898a0987b3803a6987bb2
me990928 <me990928@gmail.com>
2024-08-28 06:41:02 +0900

    add viewmodel & method

--- MyHachiPhone/Settings/SettingsViewModel.swift
+++ MyHachiPhone/Settings/SettingsViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class SettingsViewModel:ObservableObject {
+    func editWage(wage: String, completed: @escaping ()->Void) {
+        guard let wage = Int(wage) else {
+            UserDefaultsMan().saveUserDefault(value: "1100", key: "wage")
+            return completed()
+        }
+        UserDefaultsMan().saveUserDefault(value: wage, key: "wage")
+        completed()
+    }
+    
+    func editTeate(teate: Bool) {
+        UserDefaultsMan().saveUserDefault(value: teate, key: "teate")
+    }
+}
