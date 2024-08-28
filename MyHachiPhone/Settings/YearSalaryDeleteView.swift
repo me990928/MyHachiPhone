c224a5898de6b18172849657840b7db7dcf3eb6e
me990928 <me990928@gmail.com>
2024-08-29 01:42:38 +0900

    Adding a Custom List

--- MyHachiPhone/Settings/YearSalaryDeleteView.swift
+++ MyHachiPhone/Settings/YearSalaryDeleteView.swift
@@ -14,8 +14,34 @@ struct YearSalaryDeleteView: View {
     @Query private var plans: [ShiftPlans]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            if isView {
+                VStack{
+                    VStack(alignment: .leading){
+                        ForEach(yearSalaryDeleteModel.yearList, id: \.self) { year in
+                            ListChild(rowCount: $rowCount, text: year, last: yearSalaryDeleteModel.yearList.last == year)
+                        }
+                    }.padding()
+                        .onAppear(){
+                            if rowCount == 0 {
+                                isView = false
+                            }
+                        }
+                        .onChange(of: rowCount) {
+                        if rowCount == 0 {
+                            withAnimation {
+                                isView = false
+                            }
+                        }
+                    }
+                    HStack{Spacer()}
+                }.background(colorScheme == .light ? Color(.systemGray5) : Color(.systemGray2)).clipShape(RoundedRectangle(cornerRadius: 25.0))
+            }
+        }.padding().onAppear(){
+            rowCount = yearSalaryDeleteModel.yearList.count
+        }
     }
+    
 }
 
 #Preview {
