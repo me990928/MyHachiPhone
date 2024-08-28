28c0b1ec036eb595953f254ca297598bae4363c0
me990928 <me990928@gmail.com>
2024-08-29 01:41:35 +0900

    Get year array

--- MyHachiPhone/Settings/YearsSalaryDeleteModel.swift
+++ MyHachiPhone/Settings/YearsSalaryDeleteModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+class YearsSalaryDeleteModel {
+    var yearList: [String] = []
+    init(salary: [SalaryData]){
+        for i in salary {
+            let year = DateTranslator().dateComponents(i.startTime).year?.description ?? ""
+            
+            if yearList.isEmpty || yearList.last != year {
+                yearList.append(year)
+            }
+        }
+    }
+}
